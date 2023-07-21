4c6012e96b79fa9a5e9dfdcea9c1db8a05c5eabd
22026038 <22026038@DESKTOP-926IEDO>
2023-07-21 13:51:22 +0800

    update Menu Class (toString) v2

--- GA/src/Menu.java
+++ GA/src/Menu.java
@@ -7,32 +7,37 @@
  *
  */
 public class Menu {
-	private String menu_id;
+	private int menu_id;
 	private String menu_name;
 	private String menu_status;
-	private String item_id;
+	private int item_id;
 	private String item_name;
 	private String item_description;
 	private double item_price;
 	
-	public Menu(String menu_id, String menu_name, String menu_status) {
+	public Menu(int menu_id, String menu_name, String menu_status) {
 		this.menu_id = menu_id;
 		this.menu_name = menu_name;
 		this.menu_status = menu_status;
 	}
 
-	public Menu(String item_id, String item_name, String item_description, double item_price) {
+	public Menu(int item_id, String item_name, String item_description, double item_price) {
 		this.item_id = item_id;
 		this.item_name = item_name;
 		this.item_description = item_description;
 		this.item_price = item_price;
 	}
+	
+	public String toString() {
+		return String.format("%-10d %-10s %-10s %-10d %-10s %-10s %.2f", getMenu_id(), getMenu_name(), getMenu_status(), getItem_id(), getItem_name(), getItem_description(), getItem_price() );
+		
+	}
 
-	public String getMenu_id() {
+	public int getMenu_id() {
 		return menu_id;
 	}
 
-	public void setMenu_id(String menu_id) {
+	public void setMenu_id(int menu_id) {
 		this.menu_id = menu_id;
 	}
 
@@ -52,11 +57,11 @@ public class Menu {
 		this.menu_status = menu_status;
 	}
 
-	public String getItem_id() {
+	public int getItem_id() {
 		return item_id;
 	}
 
-	public void setItem_id(String item_id) {
+	public void setItem_id(int item_id) {
 		this.item_id = item_id;
 	}
 
