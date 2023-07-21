5c5b2e27160e2fde6535b6d2ee42b4d4ec07f5f0
matth_7fe0jce <matth_7fe0jce@LAPTOP-HBSN5I6G>
2023-07-21 13:42:34 +0800

    user class

--- GA/src/User.java
+++ GA/src/User.java
@@ -1,4 +1,34 @@
 
 public class User {
 
+	private String id;
+	private String Role;
+	
+	public User(String id, String role) {
+		
+		this.id = id;
+		Role = role;
+	}
+	public String toString() {
+		return String.format("%-10s %-30s", getId(), getRole());
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return Role;
+	}
+
+	public void setRole(String role) {
+		Role = role;
+	}
+	
+	
 }
