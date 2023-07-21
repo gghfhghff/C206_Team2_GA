3454c9db6c521af106d4adb593eaecb2de8cebff
22020454 <22020454@LAPTOP-SLKQ1P7M>
2023-07-21 13:58:28 +0800

    asdasdas

--- GA/src/School.java
+++ GA/src/School.java
@@ -25,15 +25,19 @@ public class School {
 	public int getSchool_id() {
 		return school_id;
 	}
+	
 	public void setSchool_id(int school_id) {
 		this.school_id = school_id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getAddress() {
 		return address;
 	}
