e6b9e3c5adf31d1d1a3c40c977cb5b1db0ec6ad3
22020454 <22020454@LAPTOP-SLKQ1P7M>
2023-07-21 14:06:24 +0800

    asjdhnaby8dgqi

--- GA/src/Payment.java
+++ GA/src/Payment.java
@@ -1,40 +1,70 @@
 
 public class Payment {
 	
-	private String Name;
-	private int id;
+	private String name;
+	private int cardNo;
+	private int cvc;
+	private String expDate;
+	
+	public Payment(String name, int cardNo, int cvc, String expDate) {
+		super();
+		this.name = name;
+		this.cardNo = cardNo;
+		this.cvc = cvc;
+		this.expDate = expDate;
+	}
+
+	
 	
 	public String toString() {
 		
-		return String.format("%-10s %-10d ",getName(), getId());
+		return String.format("%-10s %-10d %-10d %-10s", getName(), getCardNo(), getCvc(), getExpDate());
 
 	}
+
+
 	
-	
-	
-	public Payment(String name, int id) {
-		Name = name;
-		this.id = id;
-	}
-	
+
 	public String getName() {
-		return Name;
+		return name;
 	}
-	
+
+
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
 	}
-	
-	public int getId() {
-		return id;
+
+
+	public int getCardNo() {
+		return cardNo;
+	}
+
+
+	public void setCardNo(int cardNo) {
+		this.cardNo = cardNo;
+	}
+
+
+	public int getCvc() {
+		return cvc;
+	}
+
+
+	public void setCvc(int cvc) {
+		this.cvc = cvc;
+	}
+
+
+	public String getExpDate() {
+		return expDate;
+	}
+
+
+	public void setExpDate(String expDate) {
+		this.expDate = expDate;
 	}
 	
 
-
-	public void setId(int id) {
-		this.id = id;
-	}
-	
 	
 	
 
